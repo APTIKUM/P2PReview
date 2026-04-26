8b504bc8c240cbb4089af86a28835529c37efdc6
Cherkasov Vladislav <vc615774@gmail.com>
2026-04-26 06:33:41 +0500

    rework GetUserProfileAsync remove GetUserProfileQuery

--- P2PReview.Application/Interfaces/IUserService.cs
+++ P2PReview.Application/Interfaces/IUserService.cs
@@ -1,10 +1,11 @@
 ﻿using P2PReview.Application.User.DTOs;
-using P2PReview.Application.User.Queries;
 
 namespace P2PReview.Application.Interfaces
 {
     public interface IUserService
     {
-        public Task<UserProfileDto?> GetUserProfileAsync(GetUserProfileQuery query);
+        public Task<UserProfileDto?> GetUserProfileAsync(Guid userId);
+
+        public Task<Guid?> GetAuthUserId();
     }
 }
--- P2PReview.Infrastructure/Services/UserService.cs
+++ P2PReview.Infrastructure/Services/UserService.cs
@@ -1,38 +1,52 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 using P2PReview.Application.Interfaces;
 using P2PReview.Application.User.DTOs;
-using P2PReview.Application.User.Queries;
 using P2PReview.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Security.Claims;
 
 namespace P2PReview.Infrastructure.Services
 {
     public class UserService : IUserService
     {
         private readonly UserManager<User> _userManager;
+        private readonly AuthenticationStateProvider _authStateProvider;
 
-        public UserService(UserManager<User> userManager)
+        public UserService(UserManager<User> userManager,
+            AuthenticationStateProvider authenticationState)
         {
             _userManager = userManager;
+            _authStateProvider = authenticationState;
         }
 
-        public async Task<UserProfileDto?> GetUserProfileAsync(GetUserProfileQuery query)
+        public async Task<Guid?> GetAuthUserId()
         {
-            var user = await _userManager.FindByIdAsync(query.UserId.ToString());
+            var authState = await _authStateProvider.GetAuthenticationStateAsync();
+
+            var user = authState.User;
+
+            var currentUserIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            return Guid.TryParse(currentUserIdClaim, out var id) ? id : null;
+        }
+
+        public async Task<UserProfileDto?> GetUserProfileAsync(Guid userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId.ToString());
 
             if (user == null)
             {
                 return null;
             }
 
+            var authUserId = await GetAuthUserId();
+
             return new UserProfileDto()
             {
                 Id = user.Id,
                 UserName = user.UserName,
                 QualityScore = user.QualityScore,
-                IsOwnProfile = query.UserId == query.UserId
+                IsOwnProfile = userId == authUserId
             };
         }
     }
