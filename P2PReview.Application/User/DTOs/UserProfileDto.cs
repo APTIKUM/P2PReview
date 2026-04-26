53a57b7b4ca30beb5d32eb59e7e98d3faddcbb3b
Cherkasov Vladislav <vc615774@gmail.com>
2026-04-26 08:06:35 +0500

    add fields in UserProfileDto

--- P2PReview.Application/User/DTOs/UserProfileDto.cs
+++ P2PReview.Application/User/DTOs/UserProfileDto.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace P2PReview.Application.User.DTOs
+﻿namespace P2PReview.Application.User.DTOs
 {
     public class UserProfileDto
     {
         public string Id { get; set; } = null!;
         public string UserName { get; set; } = null!;
         public string AvatarUrl { get; set; } = null!;
-
         public int QualityScore { get; set; }
+        public bool IsOwnProfile { get; set; } 
 
-        public bool IsOwnProfile { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int ReviewsGiven { get; set; }
+        public int ReviewsEasy { get; set; }
+        public int ReviewsNormal { get; set; }
+        public int ReviewsHard { get; set; }
+
     }
 }
